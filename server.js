97418ce967c472fc64ef53908b28d26aaa556b65
Princewill Jeremiah <princewilljeremiah83@gmail.com>
2025-01-15 17:01:39 +0100

    Fixed ESLint config and global variables

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.get("/", (req, res) => {
 });
 
 // listen to port
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
